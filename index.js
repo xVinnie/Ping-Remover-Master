6735c9ee9a8c19b6bad665c7820255158569680b
Memeboy <jeries@outlook.de>
2017-10-22 12:41:04 -0700

    Updated Valk Lengths

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 
 module.exports = function PingRemove(dispatch) {
 
-	function PingRemove(distance)	{
-		dispatch.toServer('C_SET_VISIBLE_RANGE', {range: distance});
+	function PingRemove()	{
+		dispatch.toServer('C_REQUEST_MEMES', 1);
 	}
 
-    dispatch.hook('C_CHAT', 1, function(event) {
-		let command = event.message.replace(/<\/?[^<>]*>/gi, '').split(' ')
-		if(command[0] === '!range') {
-			if (command.length > 1) {
-				PingRemove(parseInt(command[1]))
-			}
-			return false;		
-		}
-	})
-
-}
+    
