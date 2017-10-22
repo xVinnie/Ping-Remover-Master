fa42d18766705b4d60de39687b1c2c5d4c6da459
Memeboy <jeries@outlook.de>
2017-10-22 10:36:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+module.exports = function PingRemove(dispatch) {
+
+	function PingRemove(distance)	{
+		dispatch.toServer('C_SET_VISIBLE_RANGE', {range: distance});
+	}
+
+    dispatch.hook('C_CHAT', 1, function(event) {
+		let command = event.message.replace(/<\/?[^<>]*>/gi, '').split(' ')
+		if(command[0] === '!range') {
+			if (command.length > 1) {
+				PingRemove(parseInt(command[1]))
+			}
+			return false;		
+		}
+	})
+
+}
